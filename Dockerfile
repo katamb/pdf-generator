5a246e9de98b9d5d6a22d0a76714314810396c88
katamb <katamb@ttu.ee>
2020-04-23 19:40:06 +0300

    Docker improvements

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM openjdk:13
+
 EXPOSE 7701
+
+COPY build/libs/main-app-0.1.jar main-app-0.1.jar
+
 ENTRYPOINT ["java","-jar","main-app-0.1.jar"]
-ADD build/libs/main-app-0.1.jar main-app-0.1.jar
--- src/main/java/generate/pdf/openpdf/security/TokenService.java
+++ src/main/java/generate/pdf/openpdf/security/TokenService.java
@@ -2,17 +2,13 @@ package generate.pdf.openpdf.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import generate.pdf.openpdf.dto.JwtDto;
 import generate.pdf.openpdf.exception.InternalServerException;
 import generate.pdf.openpdf.exception.UnauthorizedException;
 import generate.pdf.openpdf.mapper.UserRoleMapper;
-import generate.pdf.openpdf.service.UserFileService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
@@ -26,12 +22,7 @@ import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 
 @Service
 @RequiredArgsConstructor
